6848262cf76660af27fafa290b0d269d38d6cb10
HelloWor1d <1034597776@qq.com>
2016-11-26 18:01:25 +0800

    把server.js封装为 start 放到index.js 文件中

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 let http = require("http")
-function  onRequest (req,res) {
-    console.log("收到请求")
-        res.writeHead (200, {"Content-Type": "text/plain"})
+
+function start() {
+    function onRequest(req, res) {
+        console.log("收到请求")
+        res.writeHead(200, {
+            "Content-Type": "text/plain"
+        })
         res.write("Hello, amazing node");
         res.end();
-}
-http.createServer ( onRequest).listen(8888)
+    }
+
+    http.createServer(onRequest).listen(8888)
 console.log("服务已启动")
+}
+exports.start = start
