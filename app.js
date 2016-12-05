16d61f4f471978995540ac8c1953af6ca144af0a
HelloWor1d <1034597776@qq.com>
2016-12-05 14:42:02 +0800

    修改app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var handle= {}
 handle['/'] = requestHandler.start
 handle['/start'] = requestHandler.start
 handle['/upload'] = requestHandler.upload
+handle['/show'] = requestHandler.show
 server.start(router.route,handle)
+
 
 
                     
