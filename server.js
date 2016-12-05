be5393b5cf3d32b27a6ee360da669190304033d8
HelloWor1d <1034597776@qq.com>
2016-12-05 14:59:38 +0800

    去除postData,采用req一路传递

--- server.js
+++ server.js
@@ -7,23 +7,10 @@ function start(route,handle) {
             var postData =""
             var pathname = url.parse(req.url).pathname
             console.log("收到来自 " + pathname +" 的请求")
-
-            req.setEncoding("utf8");
-
-                req.addListener("data", function(postDataChunk) {
-                postData += postDataChunk;
-                console.log("Received POST data chunk '"+
-                postDataChunk + "'.");
-            });
-
-            req.addListener("end", function(){
-                route(pathname,handle, res, postData)
-            })
-
+                route(pathname,handle, res, req)
         }
 
     http.createServer(onRequest).listen(8888)           
-console.log("服务已启动")
+    console.log("服务已启动")
 }
 exports.start = start
-//貌似linuxmint版本没有上传成功
